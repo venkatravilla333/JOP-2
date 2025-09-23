a6cbf0bbf484530b6c034639619d4b6fc01bb247
venkat ravilla <venkatravilla333@gmail.com>
2025-09-23 15:40:54 +0530

    arrays

---
+++ 7.Arrays/index.js
@@ -0,0 +1,24 @@
+
+
+var arr = [1, 2, 3, 4, 5]
+
+// console.log(arr.length)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+
+//add
+
+// arr.unshift(0)
+// arr.push(6)
+// arr.splice(0, 0, 'hi')
+// arr.splice(arr.length, 0, 'hi')
+// console.log(arr)
+// console.log(arr)
+// arr.shift()
+// arr.pop()
+// arr.splice(2, 1)
+// console.log(arr)

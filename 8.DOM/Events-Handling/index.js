4b6ee37ff41b08dfc978ecfa1cf4b98814565208
venkat ravilla <venkatravilla333@gmail.com>
2025-09-25 14:09:37 +0530

    event bubling vs event capturing

---
+++ 8.DOM/Events-Handling/index.js
@@ -0,0 +1,42 @@
+
+
+// var parent = document.getElementById('parent')
+// var child = document.getElementById('child')
+// var subchild = document.getElementById('subchild')
+
+// function parentFun(event) {
+//   console.log('parent')
+//   // console.log(event)
+// }
+
+// parent.addEventListener('click', parentFun)
+
+// function childFun(event) {
+//   console.log('child')
+//   // console.log(event)
+// }
+
+// child.addEventListener('click', childFun)
+
+// function subChildtFun(event) {
+//   console.log('sub child')
+//   console.log(event)
+//   console.log(event.target)
+//   event.stopPropagation()
+//   // event.preventDefault()
+//   console.log(event.clientX)
+//   console.log(event.clientY)
+//   console.log(event.offsetX)
+//   console.log(event.offsetY)
+// }
+
+// subchild.addEventListener('click', subChildtFun)
+
+
+// var table = document.getElementById('table')
+
+// function tableClick(e) {
+//   console.log('click')
+//   console.log(e.target)
+// }
+// table.addEventListener('click', tableClick)
